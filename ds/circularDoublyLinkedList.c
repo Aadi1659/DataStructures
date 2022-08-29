a0d884a80a33c5ea1af7ea9afe12c8e015b2bfc1
Aaditya Dev Sharma <90966493+Aadi1659@users.noreply.github.com>
2022-08-29 17:13:06 +0530

    V 0.0.4 Date: 29/08/2022
    Added insert rear, delete front, delete rear functions.

--- ds/circularDoublyLinkedList.c
+++ ds/circularDoublyLinkedList.c
@@ -25,16 +25,17 @@ NODE * getnode(int element){
 }
 
 void display(CDLINK *ptr){
-    NODE *aux;
+    NODE *aux=ptr->head;
     if (ptr->head==NULL) {
         printf("Empty list cannot display anything\n");
     }
     else {
-        while (aux->rlink!=aux) {
+        while (aux->rlink!=ptr->head) {
             printf("%d\n",aux->data);
             aux = aux->rlink;
         }
         printf("%d\n",aux->data);
+
     }
 }
 
@@ -47,12 +48,82 @@ void insertFront(CDLINK *ptr,int element){
         ptr->head = newnode;
     }
     else {
-     
+        NODE *first=ptr->head;
+        NODE *last = ptr->head->llink;
         newnode->rlink = ptr->head;
         newnode->llink = ptr->head->llink;
+        last->rlink = newnode;
+        first->llink = newnode;
         ptr->head = newnode;
     }
 }
+
+void insertRear(CDLINK *ptr,int element){
+    NODE *newnode = getnode(element);
+    if (ptr->head==NULL)
+    {
+        newnode->llink = newnode;
+        newnode->rlink=newnode;
+        ptr->head = newnode;
+    }
+    else{
+        NODE *last = ptr->head->llink;
+        newnode->llink = last;
+        newnode->rlink = ptr->head;
+        last->rlink = newnode;
+        ptr->head->llink = newnode;
+
+    }
+}
+
+void deleteFront(CDLINK *ptr){
+    NODE *aux=ptr->head;
+    if (ptr->head==NULL)
+    {
+        printf("The list is empty cannot delete\n");
+    }
+    else if (aux->rlink==ptr->head && aux->llink==ptr->head)
+    {
+        printf("Deleted node is %d\n",aux->data);
+        ptr->head = NULL;
+        free(aux);
+    }
+    
+    else{
+        NODE *temp=ptr->head;
+        NODE *second = ptr->head->rlink;
+        NODE *last = ptr->head->llink;
+        last->rlink=second;
+        second->llink = last;
+        ptr->head = second;
+        printf("The deleted node is %d\n",temp->data);
+        free(temp);
+    }
+}
+
+void deleteRear(CDLINK *ptr){
+    NODE *aux=ptr->head;
+    if (ptr->head==NULL)
+    {
+        printf("The list is empty cannot delete\n");
+    }
+    else if (aux->rlink==ptr->head && aux->llink==ptr->head)
+    {
+        printf("Deleted node is %d\n",aux->data);
+        ptr->head = NULL;
+        free(aux);
+    }
+    else{
+        NODE *last = ptr->head->llink;
+        NODE *secondLast = last->llink;
+        secondLast->rlink = ptr->head;
+        ptr->head->llink = secondLast;
+        printf("The deleted node is %d\n",last->data);
+        free(last);
+    }
+}
+    
+    
 
 int main(){
     CDLINK cd1;
@@ -60,7 +131,11 @@ int main(){
     insertFront(&cd1,10);
     insertFront(&cd1,20);
     insertFront(&cd1,30);
-
+    insertRear(&cd1,40);
+    deleteFront(&cd1);
+    deleteRear(&cd1);
 
     display(&cd1);
+    
+
 }
